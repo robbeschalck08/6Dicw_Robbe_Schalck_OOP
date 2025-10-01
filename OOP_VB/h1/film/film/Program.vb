b59a2d66ff38f8bdc0f2651c48802fc3ca1a3f2b
robbeschalck <robbe.schalck@sfevergem.be>
2025-10-01 17:47:52 +0200

    oefening

---
+++ OOP_VB/h1/film/film/Program.vb
@@ -0,0 +1,15 @@
+Imports System
+
+Module Program
+
+    Public Class Film
+        Public strTitel As String
+        Public intJaar As Integer
+        Public intDuur As Integer
+        Public blnActie As Boolean
+    End Class
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        Dim objFilm As New Film()
+    End Sub
+End Module
