1d78a83797a11880a8586c18c0495490dfa520e4
robbeschalck <robbe.schalck@sfevergem.be>
2026-01-16 12:02:17 +0100

    toets

---
+++ C#/3lagenmodel_test_h3_Schalck_Robbe/3lagenmodel_test_h3_Schalck_Robbe/ComedyShow.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3lagenmodel_test_h3_Schalck_Robbe
+{
+    internal class ComedyShow
+    {
+        private string _naam;
+        private int _show;
+
+        public ComedyShow()
+        {
+            _naam = "...";
+            _show = 0;
+        }
+
+        public string Naam
+        {
+            get { return _naam; }
+            set { _naam = value; }
+        }
+
+        public int Show
+        {
+            get { return _show; }
+            set { _show = value; }
+        }
+
+        public double BerekenGemiddelde(double _score)
+        {
+            int _aantal = 0;
+            _aantal += 1;
+            double _gemiddelde = _score / _aantal;
+            return _gemiddelde;
+        }
+
+    }
+}
